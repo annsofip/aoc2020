1b06f2eca403cd34773d383b2be5a59fd80006a8
Annsofi Pettersson <Annsofi.Pettersson@evry.com>
2020-12-01 06:53:07 +0100

    Some optimization for day 01

--- src/main/groovy/day01/main.groovy
+++ src/main/groovy/day01/main.groovy
@@ -5,25 +5,23 @@ def numbers = new File("input.txt").readLines().collect({
 })
 
 def answer = 0
+
+//Part one
+numbers.each { n ->
+    def m = 2020 - n
+    if (numbers.contains(m)) {
+        answer = n * m
+    }
+}
+println "Two numbers: $answer"
+
+//Part two
 numbers.each { n ->
     numbers.each { m ->
-        if (n + m == 2020) {
-            answer = n * m
+        def l = 2020 - m - n
+        if (numbers.contains(l)) {
+            answer = n * m * l
         }
     }
 }
-println "Two numbrs: $answer"
-
-numbers.each { n ->
-    numbers.each { m ->
-        numbers.each { l ->
-            if (n + m + l == 2020) {
-                answer = n * m * l
-            }
-        }
-    }
-}
-
-println "Three numbrs: $answer"
-
-
+println "Three numbers: $answer"
---
+++ src/main/groovy/day02/main.groovy
@@ -0,0 +1,29 @@
+package day02
+
+def numbers = new File("input.txt").readLines().collect({
+    it.toInteger()
+})
+
+def answer = 0
+numbers.each { n ->
+    numbers.each { m ->
+        if (n + m == 2020) {
+            answer = n * m
+        }
+    }
+}
+println "Two numbrs: $answer"
+
+numbers.each { n ->
+    numbers.each { m ->
+        numbers.each { l ->
+            if (n + m + l == 2020) {
+                answer = n * m * l
+            }
+        }
+    }
+}
+
+println "Three numbrs: $answer"
+
+
