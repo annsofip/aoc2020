85eb6d18558b12edc0fe41d47a45e8e6aea01a0b
Annsofi Pettersson <Annsofi.Pettersson@evry.com>
2020-12-02 07:04:41 +0100

    first draft of day2

--- src/main/groovy/day02/main.groovy
+++ src/main/groovy/day02/main.groovy
@@ -1,29 +1,36 @@
 package day02
 
-def numbers = new File("input.txt").readLines().collect({
-    it.toInteger()
-})
-
-def answer = 0
-numbers.each { n ->
-    numbers.each { m ->
-        if (n + m == 2020) {
-            answer = n * m
-        }
-    }
-}
-println "Two numbrs: $answer"
-
-numbers.each { n ->
-    numbers.each { m ->
-        numbers.each { l ->
-            if (n + m + l == 2020) {
-                answer = n * m * l
-            }
-        }
-    }
+def part1 = new File("input.txt").readLines().inject(0) { result, input ->
+    def row = input.split(" ")
+    def range = row[0].split("-")
+    def max = range[1].toInteger()
+    def min = range[0].toInteger()
+    def letter = row[1].split(":").first()
+    def password = row[2]
+    println "$letter must occur min $min times and max $max times in $password"
+    // println(min <= password.count(letter) && password.count(letter) <= max)
+    return (min <= password.count(letter) && password.count(letter) <= max) ? ++result : result
 }
 
-println "Three numbrs: $answer"
+println(part1)
+
+
+def part2 = new File("input.txt").readLines().inject(0) { result, input ->
+    def row = input.split(" ")
+    def range = row[0].split("-")
+    def maxIndex = range[1].toInteger() - 1
+    def minIndex = range[0].toInteger() - 1
+    def letter = row[1].split(":").first()
+    def password = row[2]
+    println "$letter must be in index $minIndex OR in index $maxIndex in $password"
+    def minTrue = password.charAt(minIndex) == letter.charAt(0) ? 1 : 0
+    println(minTrue + " " + password.charAt(minIndex) + " " + letter.charAt(0))
+    def maxTrue = password.charAt(maxIndex) == letter.charAt(0) ? 1 : 0
+    println(maxTrue + " " + password.charAt(minIndex) + " " + letter.charAt(0))
+    return (minTrue + maxTrue == 1) ? ++result : result
+}
+
+println(part2)
+
 
 
