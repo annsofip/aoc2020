fa741da9d8b8fd3a9e95edb202fa28b26985cc17
Annsofi Pettersson <Annsofi.Pettersson@evry.com>
2020-12-03 19:32:01 +0100

    Code cleanup

--- src/main/groovy/day01/javascript.js
+++ src/main/groovy/day01/javascript.js
@@ -33,23 +33,3 @@ let arr = input.split("\n").map(Number);
 //console.log(arr);
 console.log(getPart1(arr));
 console.log(getPart2(arr));
-
-
-
-
-
-
-/*function getThreeNumbersWithReduce(numbers) {
-    return numbers.reduce(function (acc, n) {
-        return numbers.reduce(function (acc, m) {
-            const diff = 2020 - n - m;
-            if (numbers.includes(diff)) {
-                console.log("I Am here")
-                return n * m * diff;
-            }
-            return acc;
-        }, 0);
-
-    }, 0);
-}
-console.log(getThreeNumbersWithReduce(arr));*/
--- src/main/groovy/day02/main.groovy
+++ src/main/groovy/day02/main.groovy
@@ -4,8 +4,8 @@ static int getPart1(List<String> rows) {
     return rows.inject(0) { result, input ->
         def row = input.split(" ")
         def range = row[0].split("-")
-        def max = range[1].toInteger()
         def min = range[0].toInteger()
+        def max = range[1].toInteger()
         def letter = row[1].split(":").first()
         def password = row[2]
         //println "$letter must occur min $min times and max $max times in $password"
@@ -22,12 +22,11 @@ static int getPart2(List<String> rows) {
         def letter = row[1].split(":").first()
         def password = row[2]
         //println "$letter must be in index $minIndex OR in index $maxIndex in $password"
-        def minTrue = password.charAt(minIndex) == letter.charAt(0) ? 1 : 0
-        def maxTrue = password.charAt(maxIndex) == letter.charAt(0) ? 1 : 0
-        return (minTrue + maxTrue == 1) ? ++result : result
+        return password.charAt(minIndex) == letter.charAt(0) ^ password.charAt(maxIndex) == letter.charAt(0) ?
+                ++result :
+                result
     }
 }
-
 
 
 static void main(String[] args) {
