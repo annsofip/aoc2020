577d91a3ceb87d2d4cd1b03843620b55b1fcd850
Annsofi Pettersson <Annsofi.Pettersson@evry.com>
2020-12-06 12:57:40 +0100

    FIrst draft for day06

---
+++ src/main/groovy/day06/main.groovy
@@ -0,0 +1,28 @@
+package day06
+
+
+static int getPart1(List<String> rows) {
+    return rows.collect({
+        it.replace("\n", "")
+    }).collect({ String row -> row.toSet().sort().join().length() }).sum()
+}
+
+static int getPart2(List<String> input) {
+    return (Integer) input.collect({ group ->
+        def person = group.split("\n")
+        person.collect({ answers ->
+            answers.findAll({ answer ->
+               group.count(answer) == person.size()
+            })
+        }).flatten().unique().size()
+    }).sum()
+}
+
+static void main(String[] args) {
+    List<String> rows = new File("input.txt").getText().split("\\n\\n")
+
+    //println(rows)
+    println getPart1(rows)
+    println getPart2(rows)
+}
+
