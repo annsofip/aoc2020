c73452cb9542a07c1193c183a4720529359cf018
Annsofi Pettersson <annsofi.p@gmail.com>
2020-12-07 08:56:41 +0100

    First draft for day07 made in js

--- src/main/groovy/day07/day07.js
+++ src/main/groovy/day07/day07.js
@@ -1,16 +1,70 @@
 fs = require("fs");
 
 function getPart1(input) {
-    return input.length
+    const rules = input.reduce(function (acc, line) {
+        const split = line.split(" bags contain ");
+        acc[split[0]] = split[1].split(",").map(function (child) {
+            const bag = (/\d+ (\w+ \w+) bags?/g).exec(child);
+            return bag ? bag[1] : undefined;
+        }).filter(bag => bag);
+
+        return acc;
+    }, {});
+
+
+    let lokingFor = ["shiny gold"];
+    let result = [];
+    let searchQue = [];
+    do {
+        searchQue = [];
+        for (const i of lokingFor) {
+            //console.log("Looking for: ", i);
+            for (const j of Object.keys(rules)) {
+                //console.log("Going through: ", j);
+                if (rules[j].includes(i)) {
+                    //console.log(rules[j], i);
+                    searchQue.push(j);
+                    result.push(j);
+                }
+            }
+        }
+        lokingFor = searchQue;
+    } while (searchQue.length > 0);
+
+    return new Set(result).size;
 }
 
 function getPart2(input) {
-    return input.length
+    const rules = input.reduce(function (acc, line) {
+        const split = line.split(" bags contain ");
+        acc[split[0]] = split[1].split(",").map(function (child) {
+            const bag = (/(\d+) (\w+ \w+) bags?/g).exec(child);
+            //bag ? console.log(bag) : undefined;
+            return bag ? {type: bag[2], count: +bag[1]} : undefined;
+        }).filter(bag => bag);
+        return acc;
+    }, {});
+
+    //console.log("Rules", rules);
+
+    function getCountFor(bag) {
+        let totalCount = 0;
+        //console.log("Bag:", bag);
+        if (rules[bag]) {
+            //console.log("rules[bag]:", rules[bag]);
+            rules[bag].forEach(({type, count}) => {
+                totalCount += count + getCountFor(type) * count;
+            });
+        }
+        return totalCount;
+    }
+    return getCountFor("shiny gold");
 }
 
+
 const input = fs.readFileSync("./input.txt").toString('utf-8');
-const arr = input.split(/\n\n/);
+const arr = input.split(/\n/);
 //console.log(arr);
-console.log(getPart1(arr));
-console.log(getPart2(arr));
+console.log("PART 1:", getPart1(arr));
+console.log("PART 2:",getPart2(arr));
 
