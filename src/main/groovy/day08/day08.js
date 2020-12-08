f5b8919b988c7b7d3e69189b7a4831f286ffd377
Annsofi Pettersson <Annsofi.Pettersson@evry.com>
2020-12-08 07:48:36 +0100

    Removed logging in day08

--- src/main/groovy/day08/day08.js
+++ src/main/groovy/day08/day08.js
@@ -1,41 +1,9 @@
 fs = require("fs");
 
 function getPart1(input) {
-    let visitedIndicies = [];
-    let current = 0;
-    let acc = 0;
-    while (current < input.length) {
-        visitedIndicies.push(current);
-
-        //  console.log(input[current]);
-        const splitLine = input[current].split(" ");
-        //  console.log(splitLine);
-
-        if (splitLine[0] === "nop") {
-            current++;
-            //   console.log("Nop:", acc);
-        } else if (splitLine[0] === "acc") {
-            current++;
-            acc += parseInt(+splitLine[1]);
-            //    console.log("Acc:", acc);
-        } else if (splitLine[0] === "jmp") {
-            //   console.log("jmp:", +splitLine[1]);
-            let newIndex = current + parseInt(splitLine[1]);
-            //  console.log("newIndex:", newIndex);
-
-            if (visitedIndicies.includes(newIndex)) {
-                break;
-            } else {
-                current = newIndex;
-            }
-
-        }
-        //   console.log("--------------------------------")
-
-    }
-
-    //  console.log("Visited:", visitedIndicies);
-    return acc
+    const instructions = getInstructions(input);
+    let {acc} = runProgram(instructions, input);
+    return acc;
 }
 
 function runProgram(instructions) {
@@ -46,49 +14,41 @@ function runProgram(instructions) {
     while (current < instructions.length) {
         visitedIndicies.push(current);
         const instruction = instructions[current];
-        console.log(instruction);
         if (instruction.action === "nop") {
             current++;
-            //   console.log("Nop:", acc);
         } else if (instruction.action === "acc") {
             current++;
             acc += parseInt(instruction.count);
-            //    console.log("Acc:", acc);
         } else if (instruction.action === "jmp") {
-            //   console.log("jmp:", +splitLine[1]);
             let newIndex = current + parseInt(instruction.count);
-            //  console.log("newIndex:", newIndex);
-
             if (visitedIndicies.includes(newIndex)) {
                 break;
             } else {
                 current = newIndex;
             }
-
         } else {
-            console.log("SUCCESS!!");
             success = true;
             break;
         }
-        console.log("--------------------------------")
-
     }
     return {acc: acc, finished: success};
 }
 
-function getPart2(input) {
-
-    const instructions = input.map(function (line) {
+function getInstructions(input) {
+    return input.map(function (line) {
         const splitLine = line.split(" ");
         return {
             action: splitLine[0],
             count: parseInt(splitLine[1])
         }
     });
+}
 
+function getPart2(input) {
+    const instructions = getInstructions(input);
 
-    let changedInstruction = [];
     let acc = 0;
+    let changedInstruction = [];
     let finished = false;
     while (!finished) {
         let changed = false;
@@ -96,7 +56,6 @@ function getPart2(input) {
             if (!changed && instruction.action === "jmp" && !changedInstruction.includes(index)) {
                 changedInstruction.push(index);
                 changed = true;
-                console.log("Changing", index);
                 return {
                     action: "nop",
                     count: instruction.count
@@ -104,15 +63,8 @@ function getPart2(input) {
             }
             return instruction;
         });
-        console.log(newInstruction);
-        let result = runProgram(newInstruction, input);
-        finished = result.finished;
-        acc = result.acc;
-        console.log("--------------------------------")
+        ({acc, finished} = runProgram(newInstruction, input));
     }
-
-
-    //   console.log("Visited:", visitedIndicies);
     return acc
 }
 
@@ -120,6 +72,6 @@ function getPart2(input) {
 const input = fs.readFileSync("./input.txt").toString('utf-8');
 const arr = input.split(/\n/);
 //console.log(arr);
-//console.log("PART 1:", getPart1(arr));
+console.log("PART 1:", getPart1(arr));
 console.log("PART 2:", getPart2(arr));
 
